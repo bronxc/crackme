de16045b8431c12a31befa71595411d668ae15a0
Jon Robison <narfman0@gmail.com>
2017-05-19 00:31:29 -0400

    -Add WIP 2

--- 1/main.c
+++ 1/main.c
@@ -3,10 +3,23 @@
 #include <string.h>
 
 int main() {
-	char input[9];
-	fputs("Please input password (9 chars): ", stdout);
+	char input[12];
+	char pass[12];
+	fputs("Please input name: ", stdout);
 	fgets(input, sizeof input, stdin);
-	if (strcmp("hunter23", input) == 0)
+	fputs("Please input pass: ", stdout);
+	fgets(pass, sizeof pass, stdin);
+	srand(1337);
+	const unsigned char table[8] = {173,135,131,121,110,119,187,143};
+	int i;
+    for (i = 0; i < sizeof input; i++) {
+        input[i] ^= table[i % 8];
+    }
+	bool accepted = sizeof input == sizeof pass;
+    for (i = 0; accepted && i < sizeof input; i++) {
+        accepted &= input[i] == pass[i];
+    }
+	if (accepted)
 		fputs("Correct!\n", stdout);
 	else
 		fputs("Incorrect\n", stdout);
---
+++ 2/README.md
@@ -0,0 +1,22 @@
+2
+=
+
+Note: this is just a first pass :)
+
+Pretty simple project. This console app expects the user to type their own
+name, then put in a corresponding password (ostensibly emailed or whatever).
+The goal is to bypass this check and always continue to the success logic
+branch. A good exercise would be to also write a reversing mechanism.
+
+Usage
+-----
+
+Compile with gcc/visualstudio/whatever. Open in your favorite dissassembler and
+patch the executable code. Run again for the win.
+
+License
+-------
+
+Copyright (c) 2017 Jon Robison
+
+See included LICENSE for licensing information
